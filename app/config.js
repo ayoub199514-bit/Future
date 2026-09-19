6ebc8238ce0aaa2ddb5850ace47a874b5875a32f
ayoub199514-bit <ayoub.199514@gmail.com>
2026-09-19 14:05:43 +0100

    Add files via upload

---
+++ app/config.js
@@ -0,0 +1,3 @@
+// FURI Mining - Supabase configuration
+window.FURI_SUPABASE_URL = "https://zkmvqjydghpporpnoejy.supabase.co";
+window.FURI_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_i9iDtzziwGvOIAmFWi05HA_hCQpj-Ac";
